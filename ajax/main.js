8e2331fd5ab125434f06a25576b0847f981e9fe2
zll18314831310 <18314831310@163.com>
2018-06-23 11:13:41 +0800

    node ajax

---
+++ ajax/main.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const fs = require('fs');
+const indexFile = fs.createReadStream('')
+
+
+// 创建一个web 服务器
+const server = http.createServer((req,res)=>{
+    console.log('那个家伙又来了')
+    res.end('hello world')
+})
+
+server.listen(8082)
